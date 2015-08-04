59a610fa432e16dd147bff554e1f5449bfcd62aa
Steven Nguyen <ngynstvn@gmail.com>
2015-08-03 21:48:13 -0700

    CP35 - Data Source - Changed HelloWorld using findViewbyId and TextView's setText() method (Transfer)

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,6 +2,7 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.bloc.android.blocly.BloclyApplication;
@@ -13,6 +14,9 @@ public class BloclyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
+
+        TextView t = (TextView) findViewById(R.id.text_view);
+        t.setText(R.string.hello_world);
 
         Toast.makeText(this,
             BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
